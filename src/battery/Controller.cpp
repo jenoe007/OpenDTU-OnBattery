vaterlangen/OpenDTU-OnZendure

Zendure: Feature: Added support for official Zendure API

src/battery/Controller.cpp
@@ -7,7 +7,8 @@
 #include <battery/pytes/Provider.h>
 #include <battery/sbs/Provider.h>
 #include <battery/victronsmartshunt/Provider.h>
-#include <battery/zendure/Provider.h>
+#include <battery/zendure/LocalMqttProvider.h>
+#include <battery/zendure/ZendureMqttProvider.h>
 #include <Configuration.h>
 #include <LogHelper.h>
 
@@ -75,7 +76,17 @@ void Controller::updateSettings()
             _upProvider = std::make_unique<JbdBms::Provider>();
             break;
         case 7:
-            _upProvider = std::make_unique<Zendure::Provider>();
+            switch (config.Battery.Zendure.ConnectionType) {
+                case BatteryZendureConfig::ConnectionType::LocalMqtt:
+                    _upProvider = std::make_unique<Zendure::LocalMqttProvider>();
+                    break;
+                case BatteryZendureConfig::ConnectionType::ZendureMqtt:
+                    _upProvider = std::make_unique<Zendure::ZendureMqttProvider>();
+                    break;
+                default:
+                    DTU_LOGE("Unknown Zendure connection type: %d", config.Battery.Zendure.ConnectionType);
+                    return;
+            }
             break;
         default:
             DTU_LOGE("Unknown provider: %d", config.Battery.Provider);

jenoe007/OpenDTU-OnBattery

src/battery/Controller.cpp
@@ -7,7 +7,8 @@
 #include <battery/pytes/Provider.h>
 #include <battery/sbs/Provider.h>
 #include <battery/victronsmartshunt/Provider.h>
-#include <battery/zendure/Provider.h>
+#include <battery/zendure/LocalMqttProvider.h>
+#include <battery/zendure/ZendureMqttProvider.h>
 #include <Configuration.h>
 #include <LogHelper.h>
 
@@ -76,7 +77,17 @@ void Controller::updateSettings()
             _upProvider = std::make_unique<JbdBms::Provider>();
             break;
         case 7:
-            _upProvider = std::make_unique<Zendure::Provider>();
+            switch (config.Battery.Zendure.ConnectionType) {
+                case BatteryZendureConfig::ConnectionType_t::LocalMqtt:
+                    _upProvider = std::make_unique<Zendure::LocalMqttProvider>();
+                    break;
+                case BatteryZendureConfig::ConnectionType_t::ZendureMqtt:
+                    _upProvider = std::make_unique<Zendure::ZendureMqttProvider>();
+                    break;
+                default:
+                    DTU_LOGE("Unknown Zendure connection type: %d", config.Battery.Zendure.ConnectionType);
+                    return;
+            }
             break;
         default:
             DTU_LOGE("Unknown provider: %d", config.Battery.Provider);